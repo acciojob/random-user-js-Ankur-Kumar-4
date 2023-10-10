026fdd95270b285aa876536e9054ba498d7a64fe
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-10-10 15:00:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,59 +1,61 @@
-//your code here
-var obj;
+let obj;
 
-function fetchAndUpdateUserData(){
-let p = fetch("https://randomuser.me/api/");
+function fetchAndUpdateUserData() {
+  let p = fetch("https://randomuser.me/api/");
 
-p.then((response) => {
-  console.log(response.status);
-  console.log(response.ok);
-  return response.json();
-}).then((value2) => {
-  console.log(value2);
-  obj = value2;
-  // Access the large picture URL from the fetched data
-  let largePictureUrl = value2.results[0].picture.large;
-  let name = value2.results[0].name.first +" "+ value2.results[0].name.last 
+  p.then((response) => {
+    console.log(response.status);
+    console.log(response.ok);
+    return response.json();
+  }).then((value2) => {
+    console.log(value2);
+    obj = value2;
+    // Access the large picture URL from the fetched data
+    let largePictureUrl = value2.results[0].picture.large;
+    let name = value2.results[0].name.first + " " + value2.results[0].name.last;
 
-  // Set the HTML content with the image URL
-  document.getElementById("img").innerHTML = `
-    <img src="${largePictureUrl}" alt="Random User Image"></img>
-  `;
+    // Set the HTML content with the image URL
+    document.getElementById("img").innerHTML = `
+      <img src="${largePictureUrl}" alt="Random User Image"></img>
+    `;
 
-  document.getElementById("name").innerHTML = `
-    ${name}
-  `;
-  document.getElementById("display").innerHTML="";
+    document.getElementById("name").innerHTML = `
+      ${name}
+    `;
 
-});
+    document.getElementById("display").innerHTML = ""; // Clear previous content
 
+  });
 }
-
 
 fetchAndUpdateUserData();
 
-document.querySelector("[data-attr=age]").addEventListener("click",()=>{
+document.querySelector("[data-attr=age]").addEventListener("click", () => {
+
+  if (obj.results[0].registered) {
     let age = obj.results[0].registered.age;
-    document.getElementById("display").innerHTML=`
-    ${age}
+    document.getElementById("display").innerHTML = `
+      Age: ${age}
     `;
+  } else {
+    
+  }
 });
 
-document.querySelector("[data-attr=email]").addEventListener("click",()=>{
-    let email = obj.results[0].email;
-    document.getElementById("display").innerHTML=`
-    ${email}
-    `;
+document.querySelector("[data-attr=email]").addEventListener("click", () => {
+  let email = obj.results[0].email;
+  document.getElementById("display").innerHTML = `
+    Email: ${email}
+  `;
 });
 
-document.querySelector("[data-attr=phone]").addEventListener("click",()=>{
-    let phone = obj.results[0].phone;
-    document.getElementById("display").innerHTML=`
-    ${phone}
-    `;
-})
+document.querySelector("[data-attr=phone]").addEventListener("click", () => {
+  let phone = obj.results[0].phone;
+  document.getElementById("display").innerHTML = `
+    Phone: ${phone}
+  `;
+});
 
-document.getElementById("getUser").addEventListener("click",()=>{
-    fetchAndUpdateUserData();
-
-})
+document.getElementById("getUser").addEventListener("click", () => {
+  fetchAndUpdateUserData();
+});
