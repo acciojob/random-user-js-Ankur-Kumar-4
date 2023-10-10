fef6fe5025b092a0d04948c083743dc522761001
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-10-10 14:50:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 //your code here
+var obj;
+
+function fetchAndUpdateUserData(){
+let p = fetch("https://randomuser.me/api/");
+
+p.then((response) => {
+  console.log(response.status);
+  console.log(response.ok);
+  return response.json();
+}).then((value2) => {
+  console.log(value2);
+  obj = value2;
+  // Access the large picture URL from the fetched data
+  let largePictureUrl = value2.results[0].picture.large;
+  let name = value2.results[0].name.first +" "+ value2.results[0].name.last 
+
+  // Set the HTML content with the image URL
+  document.getElementById("img").innerHTML = `
+    <img src="${largePictureUrl}" alt="Random User Image"></img>
+  `;
+
+  document.getElementById("name").innerHTML = `
+    ${name}
+  `;
+  document.getElementById("display").innerHTML="";
+
+});
+
+}
+
+
+fetchAndUpdateUserData();
+
+document.querySelector("[data-attr=age]").addEventListener("click",()=>{
+    let age = obj.results[0].registered.age;
+    document.getElementById("display").innerHTML=`
+    ${age}
+    `;
+});
+
+document.querySelector("[data-attr=email]").addEventListener("click",()=>{
+    let email = obj.results[0].email;
+    document.getElementById("display").innerHTML=`
+    ${email}
+    `;
+});
+
+document.querySelector("[data-attr=phone]").addEventListener("click",()=>{
+    let phone = obj.results[0].phone;
+    document.getElementById("display").innerHTML=`
+    ${phone}
+    `;
+})
+
+document.getElementById("getUser").addEventListener("click",()=>{
+    fetchAndUpdateUserData();
+
+})
